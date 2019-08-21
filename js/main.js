4f3953f4220b1407573622f95ccfe58b84d6ed5c
Agustina Coppe <agustinacoppe_@hotmail.com>
2019-08-21 15:10:25 -0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const countdown = document.querySelector(".countdown");
+const launchDate = new Date("Dec 25, 2019 00:00:00").getTime();
+
+const intvl = setInterval(() => {
+  const now = new Date().getTime();
+  const distance = launchDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  countdown.innerHTML = `
+  <div>${days}<span> Days</span></div>
+  <div>${hours}<span> Hours</span></div>
+  <div>${mins}<span> Minutes</span></div>
+  <div>${seconds}<span> Seconds</span></div>
+  `;
+
+  if (distance < 0) {
+    clearInterval(intvl);
+    countdown.style.color = `#ffffff`;
+    countdown.innerHTML = "Launched!";
+  }
+}, 1000);
